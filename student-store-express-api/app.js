7a055174fb13b539143ea4fa4e133616e1bb4390
wyneep <wp2279@barnard.edu>
2022-06-23 17:19:56 -0400

    init

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,2 +1,16 @@
-// YOUR CODE HERE
+const express = require("express")
+const morgan = require("morgan")
+const Store = require("./routes/store")
+const app = express()
 
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.get('/store', Store);
+app.get('/store/:productId', Store);
+
+app.get("/", (req, res) => {
+    res.status(200).json({"ping":"pong"})
+})
+
+module.exports = app
